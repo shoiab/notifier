39e1b9da94c03a237e15d7187d6c09f066dd2b17
Gayatri_Raghavan <gayatri.rp@compassitesinc.com>
2016-12-29 16:38:09 +0530

    forwarding mails for group and user according to recipientType

--- src/main/java/com/notifier/dataService/Impl/MailServiceImpl.java
+++ src/main/java/com/notifier/dataService/Impl/MailServiceImpl.java
@@ -21,81 +21,99 @@ import com.notifier.model.TaskRecipientModel;
 import freemarker.template.Configuration;
 
 @Service
-public class MailServiceImpl implements MailService{
-	
-	 @Autowired
-	 JavaMailSender mailSender;
-	 
-	 @Autowired
-	 Configuration freemarkerConfiguration;
+public class MailServiceImpl implements MailService {
+
+	@Autowired
+	JavaMailSender mailSender;
+
+	@Autowired
+	Configuration freemarkerConfiguration;
 
 	@Override
 	public void sendMail(TaskModel taskmodel) {
-		
-		List<TaskRecipientModel> recipientlist = taskmodel.getRecipientList();
-		for(TaskRecipientModel reciepientmodel : recipientlist){
-			MimeMessagePreparator preparator = getMessagePreparator(taskmodel, reciepientmodel);
-	         
-	        try {
-	            mailSender.send(preparator);
-	            System.out.println("Message has been sent.............................");
-	        }
-	        catch (MailException ex) {
-	            System.err.println(ex.getMessage());
-	        }
-		}
-		 
-		
-	}
-	
-	private MimeMessagePreparator getMessagePreparator(final TaskModel taskmodel, TaskRecipientModel recipientmodel){
-        
-        MimeMessagePreparator preparator = new MimeMessagePreparator() {
- 
-        	@Override
-            public void prepare(MimeMessage mimeMessage) throws Exception {
-                MimeMessageHelper helper = new MimeMessageHelper(mimeMessage, true);
-  
-                helper.setSubject("Task Notification");
-                helper.setFrom("rpgayatri@gmail.com");
-                
-                System.out.println("recipient :: "+recipientmodel.getRecipient());
-                helper.setTo(recipientmodel.getRecipient());
-      
-                Map<String, Object> templatemodel = new HashMap<String, Object>();
-                
-                Map<String, String> taskmap = new HashMap<String, String>();
-                taskmap.put("description", taskmodel.getDescription());
-                taskmap.put("title", taskmodel.getTaskTitle());
-                taskmap.put("user", recipientmodel.getRecipient());
-                
-                templatemodel.put("task", taskmap);
-   
-               /* templatemodel.put("task", taskmodel);
-                templatemodel.put("recipients", recipientmodel);*/
-             
-                String text = geFreeMarkerTemplateContent(templatemodel);
-                System.out.println("Template content : "+text);
- 
-                // use the true flag to indicate you need a multipart message
-                helper.setText(text, true);
- 
-            }
 
-        };
-        return preparator;
-    }
-	
-	public String geFreeMarkerTemplateContent(Map<String, Object> model){
-        StringBuffer content = new StringBuffer();
-        try{
-         content.append(FreeMarkerTemplateUtils.processTemplateIntoString( 
-                 freemarkerConfiguration.getTemplate("fm_mailTemplate.txt"),model));
-         return content.toString();
-        }catch(Exception e){
-            System.out.println("Exception occured while processing fmtemplate:"+e.getMessage());
-        }
-          return "";
-    }
+		List<TaskRecipientModel> recipientlist = taskmodel.getRecipientList();
+		for (TaskRecipientModel reciepientmodel : recipientlist) {
+
+			if (reciepientmodel.getRecipientType().equals("user")) {
+				String recipientEmail = reciepientmodel.getRecipient();
+				MimeMessagePreparator preparator = getMessagePreparator(
+						taskmodel, reciepientmodel, recipientEmail);
+
+				try {
+					mailSender.send(preparator);
+					
+				} catch (MailException ex) {
+					System.err.println(ex.getMessage());
+				}
+			} else {
+				String[] emailarr = reciepientmodel.getRecipient().split(",");
+				for (String email : emailarr) {
+					MimeMessagePreparator preparator = getMessagePreparator(
+							taskmodel, reciepientmodel, email);
+
+					try {
+						mailSender.send(preparator);
+					} catch (MailException ex) {
+						System.err.println(ex.getMessage());
+					}
+				}
+			}
+
+		}
+
+	}
+
+	private MimeMessagePreparator getMessagePreparator(
+			final TaskModel taskmodel, TaskRecipientModel recipientmodel,
+			String recipientEmail) {
+
+		MimeMessagePreparator preparator = new MimeMessagePreparator() {
+
+			@Override
+			public void prepare(MimeMessage mimeMessage) throws Exception {
+
+				MimeMessageHelper helper = new MimeMessageHelper(mimeMessage,
+						true);
+				helper.setSubject("Task Notification");
+				helper.setFrom(taskmodel.getTaskCreator());
+				System.out.println("recipient :: " + recipientEmail);
+				helper.setTo(recipientEmail);
+
+				Map<String, Object> templatemodel = new HashMap<String, Object>();
+
+				Map<String, String> taskmap = new HashMap<String, String>();
+				
+				
+				taskmap.put("taskdescription", taskmodel.getDescription());
+				taskmap.put("title", taskmodel.getTaskTitle());
+				taskmap.put("username", recipientEmail);
+
+				templatemodel.put("task", taskmap);
+
+				String text = geFreeMarkerTemplateContent(templatemodel);
+				// use the true flag to indicate you need a multipart message
+				helper.setText(text, true);
+
+				
+			}
+
+		};
+		return preparator;
+	}
+
+	public String geFreeMarkerTemplateContent(Map<String, Object> model) {
+		StringBuffer content = new StringBuffer();
+		try {
+			content.append(FreeMarkerTemplateUtils.processTemplateIntoString(
+					freemarkerConfiguration.getTemplate("fm_mailTemplate.txt"),
+					model));
+			return content.toString();
+		} catch (Exception e) {
+			System.out.println("Exception occured while processing fmtemplate:"
+					+ e.getMessage());
+		}
+		return "";
+	}
 
 }
